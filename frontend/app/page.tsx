e0f4af8cd01ce51da3fc8d5cec560bbc3a712d83
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2024-08-18 17:02:26 -0300

    FEAT: New home view, styled with Loybits theme

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,27 +1,9 @@
-import Card from "@/components/home/card";
 import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
+import { Twitter } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
-import { nFormatter } from "@/lib/utils";
 
 export default async function Home() {
-  const { stargazers_count: stars } = await fetch(
-    "https://api.github.com/repos/steven-tey/precedent",
-    {
-      ...(process.env.GITHUB_OAUTH_TOKEN && {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_OAUTH_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      }),
-      // data will revalidate every 24 hours
-      next: { revalidate: 86400 },
-    },
-  )
-    .then((res) => res.json())
-    .catch((e) => console.log(e));
 
   return (
     <>
@@ -54,13 +36,12 @@ export default async function Home() {
           style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
         >
           <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
+            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:border-gray-500"
             href={"/user_view"}
-            target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              src="/user.png"
+              src="/user.webp"
               alt="Logo"
               width="30"
               height="30"
@@ -71,33 +52,21 @@ export default async function Home() {
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
             href="/acquirer_view"
-            target="_blank"
             rel="noopener noreferrer"
           >
-            <Github />
+            <Image
+              src="/business.png"
+              alt="Logo"
+              width="30"
+              height="30"
+              className="mr-2 rounded-sm"
+            />
             <p>
               View Project as Business Owner
             </p>
           </a>
         </div>
       </div>
-      {/*<div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">*/}
-      {/*  {features.map(({ title, description, demo, large }) => (*/}
-      {/*    <Card*/}
-      {/*      key={title}*/}
-      {/*      title={title}*/}
-      {/*      description={description}*/}
-      {/*      demo={*/}
-      {/*        title === "Beautiful, reusable components" ? (*/}
-      {/*          <ComponentGrid />*/}
-      {/*        ) : (*/}
-      {/*          demo*/}
-      {/*        )*/}
-      {/*      }*/}
-      {/*      large={large}*/}
-      {/*    />*/}
-      {/*  ))}*/}
-      {/*</div>*/}
     </>
   );
 }
--- frontend/components/layout/footer.tsx
+++ frontend/components/layout/footer.tsx
@@ -3,10 +3,10 @@ import { BuyMeACoffee } from "../shared/icons";
 export default function Footer() {
   return (
     <div className="absolute w-full py-5 text-center">
-      <p className="text-gray-500">
+      <p className="text-gray-100">
         A project by{" "}
         <a
-          className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
+          className="font-semibold text-gray-50 underline-offset-4 transition-colors hover:underline"
           href="#"
           target="_blank"
           rel="noopener noreferrer"
